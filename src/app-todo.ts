8bf598a27788a616fc47015739519080cd77308b
Sebastian <johancs.mm@gmail.com>
2023-08-29 11:01:19 -0500

    chore: Add custom elemet

--- src/app-todo.ts
+++ src/app-todo.ts
@@ -1,14 +1,18 @@
 import { LitElement, html, css } from "lit";
 import { customElement } from "lit/decorators.js";
+import "./components/custom-element";
 
 @customElement("app-todo")
 export default class AppTodo extends LitElement {
   static styles = css`
     :host {
-      color: 00ff00;
+      color: #00ff00;
     }
   `;
   render() {
-    return html` <h1>App principal</h1> `;
+    return html`
+      <h1>App principal</h1>
+      <custom-element></custom-element>
+    `;
   }
 }
