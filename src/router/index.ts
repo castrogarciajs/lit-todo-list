9c479fd8b507bb0872c5cd83b67829528c871561
Sebastian <johancs.mm@gmail.com>
2023-09-04 23:09:24 -0500

    feat: Desing Dashboard

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,10 +3,12 @@ import "../pages/home-view";
 import "../pages/login-view";
 import "../pages/register-view";
 import "../pages/dashboard-view";
+import "../pages/dashboard-tasks-view";
 
 export const views: Route[] = [
   { path: "/", component: "home-view" },
   { path: "/login", component: "login-view" },
   { path: "/register", component: "register-view" },
-  { path: "/dashboard", component: "dasboard-view" },
+  { path: "/dashboard", component: "dashboard-view" },
+  { path: "/tasks", component: "dashboard-tasks-view" },
 ];
